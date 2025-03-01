a0962aa1c8a332e6ea1518d362fca5be0b6fa084
Rasha <ra.2021lynwood@hotmail.com>
2025-03-01 14:34:49 -0800

    remote runner

--- src/test/java/cydeo/pages/PracticePage.java
+++ src/test/java/cydeo/pages/PracticePage.java
@@ -36,8 +36,15 @@ public class PracticePage {
 
     public void login() {
 
-        String username = ConfigurationReader.getProperty("username");
-        String password = ConfigurationReader.getProperty("password");
+        //  String username = ConfigurationReader.getProperty("username");
+        //  String password = ConfigurationReader.getProperty("password");
+
+        String username = System.getenv("USERNAME");
+        String password = System.getenv("PASSWORD");
+
+        System.out.println("username: " + username);
+        System.out.println("password = " + password);
+
 
         usernameEl.sendKeys(username);
         passwordEl.sendKeys(password);
